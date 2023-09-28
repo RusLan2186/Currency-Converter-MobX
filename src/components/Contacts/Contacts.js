2b812de11ef59c701c7b1e676bc14fead60d25ae
Ruslan <ruslanchikbest@gmail.com>
2023-09-28 14:50:00 +0300

    Adding map

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -17,6 +17,16 @@ const Contacts = () => {
           <a href='mailto:cometous@gmail.com'> cometous@gmail.com</a>
         </div>
       </div>
+      <div className={cl.cart}>
+        <iframe
+          src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d638455.6028387208!2d5.68681921193368!3d51.311727787555135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1suk!2sua!4v1695306058314!5m2!1suk!2sua'
+          width='800'
+          height='600'
+          allowfullscreen=''
+          loading='lazy'
+          referrerpolicy='no-referrer-when-downgrade'
+        ></iframe>
+      </div>
     </div>
   );
 };
