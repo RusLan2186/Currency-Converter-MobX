65b6fd1062ec9c18dfca90f60298c25fe78becbd
Ruslan <ruslanchikbest@gmail.com>
2023-09-28 14:35:34 +0300

    Create logics

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Info from './Info/Info';
+import Logics from './Convertor/Logics';
 
 const Main = () => {
   return (
     <div style={{ minHeight: '100%' }}>
       <Info />
+      <Logics />
     </div>
   );
 };
