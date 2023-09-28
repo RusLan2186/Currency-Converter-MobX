65daeb1af86bd9ff8a04ea25ae1bda01abee0d29
Ruslan <ruslanchikbest@gmail.com>
2023-09-28 15:42:50 +0300

    Adding fixes

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -16,10 +16,15 @@ const Contacts = () => {
           <img src={mail} alt='mail' />
           <a href='mailto:cometous@gmail.com'> cometous@gmail.com</a>
         </div>
+        <div className={cl.adress}>
+          <a href=' https://maps.app.goo.gl/7fWutAu7p7TASHgQ7'>
+            Central Office: M R M Worldwide Ltd, London SE1 0PN
+          </a>
+        </div>
       </div>
       <div className={cl.cart}>
         <iframe
-          src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d638455.6028387208!2d5.68681921193368!3d51.311727787555135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1suk!2sua!4v1695306058314!5m2!1suk!2sua'
+          src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d23617.660297904076!2d-0.12318239141227108!3d51.5223750129429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1695903925126!5m2!1sru!2sua'
           width='800'
           height='600'
           allowfullscreen=''
