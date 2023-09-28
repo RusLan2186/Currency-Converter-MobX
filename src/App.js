dd6553c8cef694803dad9e2f055ad5987908ffa9
Ruslan <ruslanchikbest@gmail.com>
2023-09-28 13:37:16 +0300

    Adding Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Info from './components/Info/Info';
 import './scss/App.scss';
 import Container from '@mui/material/Container';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='wrapper'>
+      <header>
+        <Header />
+      </header>
       <main className='main'>
         <Container sx={{ padding: 0 }}>
           <Info />
